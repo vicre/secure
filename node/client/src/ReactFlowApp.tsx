d500108e9707d9272735378fb09eb4e7bbeef946
Victor Reipur <vicre@dtu.dk>
2026-01-14 15:04:13 +0100

    Added reactflow

--- node/client/src/ReactFlowApp.tsx
+++ node/client/src/ReactFlowApp.tsx
@@ -14,7 +14,6 @@ import "reactflow/dist/style.css";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
-  { id: "2", position: { x: 200, y: 120 }, data: { label: "Node 2" } }
 ];
 
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
--- node/client/src/main.tsx
+++ node/client/src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactFlowApp from "./ReactFlowApp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ReactFlowApp />
   </React.StrictMode>
 );
